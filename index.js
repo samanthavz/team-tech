624bc6d67cb8b65b4c1d064bc1742984d40cedb4
Jabir Tisoudali <jabirtisou@hotmail.com>
2021-06-10 11:32:48 +0200

    login works

--- index.js
+++ index.js
@@ -1,20 +1,66 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const flash = require("connect-flash");
+
 const app = express();
 const port = 3000;
-const router = require("./router")
+require("./config/passport")(passport);
+
+mongoose
+  .connect(
+    "mongodb+srv://Jabir:Teampugsjabir1@doggoapp.e9zp8.mongodb.net/DoggoApp?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
 
 // templating
-app.set("views", "views")
+app.set("views", "views");
 app.set("view engine", "pug");
 app.use(express.static(__dirname + "/static/public/"));
 
+app.use(
+  require("express-session")({
+    secret: "Any normal Word", //decode or encode session
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
 // For parsing nested JSON objects
 // see https://medium.com/@mmajdanski/express-body-parser-and-why-may-not-need-it-335803cd048c
-app.use(express.urlencoded({ extended: true })); //Parse URL-encoded bodies
-app.use(express.json());
+app.use(express.json()); //Parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
+//Passport config
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global Vars
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
+});
 
 // MVC model: https://www.youtube.com/watch?v=dDjzTDN3cy8
-app.use("/", router)
+const router = require("./router");
+app.use("/", router);
+
+//In app.js
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
 
 //connect
 app.listen(process.env.PORT || port, () => {
---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+});
+const User = mongoose.model("user", UserSchema);
+module.exports = User;
