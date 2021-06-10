301327910c000e19ea0ef652bc28860a376c7ee3
Jabir Tisoudali <jabirtisou@hotmail.com>
2021-06-10 19:43:18 +0200

    DB fix to doggoswipe

--- models/user.js
+++ models/user.js
@@ -1,26 +1,37 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
+const UserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: false,
+    },
+    img: {
+      type: String,
+      required: false,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  age: {
-    type: Number,
-    required: true,
-  },
-});
-const User = mongoose.model("user", UserSchema);
+  { collection: "Users" }
+);
+const User = mongoose.model("User", UserSchema);
 module.exports = User;
